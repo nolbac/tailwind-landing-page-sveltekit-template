0ee71110cfe85392a43b2494c281208f359f4bc8
Nolwen <nbachtle@outlook.com>
2023-12-26 17:42:03 -0500

    DaisyUI Install Attempt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#3b82f6',
+					secondary: '#a78bfa',
+					accent: '#6ee7b7',
+					neutral: '#a5b4fc',
+					'base-100': '#f5f5f4',
+					info: '#111827',
+					success: '#4ade80',
+					warning: '#eab308',
+					error: '#dc2626'
+				}
+			}
+		]
+	},
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require('daisyui')]
 };
 
 module.exports = config;
